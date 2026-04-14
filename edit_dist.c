bba34e0224a6302d399a0930d23036f4dc6148cd
San Efrime <sanefrime@gmail.com>
2026-04-14 18:02:26 +0300

    update solution

--- edit_dist.c
+++ edit_dist.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+ #include <stdio.h>
 #include <string.h>
 
 #define MAX_WORD_LEN 100
@@ -19,5 +19,43 @@ int main() {
 }
 
 int edit_dist(char word1[], char word2[]) {
-	/* YOUR CODE HERE */
+
+	    scanf("%s", word1);
+	    scanf("%s", word2);
+
+	    int n = strlen(word1);
+	    int m = strlen(word2);
+
+	    int dp[101][101];
+
+
+	    for (int i = 0; i <= n; i++)
+	        dp[i][0] = i;
+
+
+	    for (int j = 0; j <= m; j++)
+	        dp[0][j] = j;
+
+
+	    for (int i = 1; i <= n; i++) {
+	        for (int j = 1; j <= m; j++) {
+
+
+	            if (s1[i-1] == s2[j-1]) {
+	                dp[i][j] = dp[i-1][j-1];
+	            }
+
+
+	            else {
+	                dp[i][j] = 1 + min(
+	                    dp[i-1][j],
+	                    dp[i][j-1],
+	                    dp[i-1][j-1]
+	                );
+	            }
+	        }
+	    }
+
+	    printf("%d\n", dp[n][m]);
+	    return 0;
 }
